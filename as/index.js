82a069e13d0ae820ad6485dfdaa0aae79e6b379e
Jacob Steves <jacob.steves@shopify.com>
2021-05-13 18:52:51 -0400

    update as definition file

--- as/index.js
+++ as/index.js
@@ -1,11 +1,15 @@
-//@ts-ignore
+//     This file prevents compilation errors where the transform is importing a 
+// different version of AssemblyScript than the root project uses. To get around
+// this, we use the AssemblyScript dependenies found root project's node modules.
+//
+// Ref: https://github.com/AssemblyScript/assemblyscript/issues/1695
+
 const path = require("path");
 
 let ascPath = Object.getOwnPropertyNames(require.cache).filter((s) =>
   s.endsWith("asc.js")
 )[0];
 
-//@ts-ignore
 let assemblyscriptPath = Object.getOwnPropertyNames(require.cache).filter((s) =>
   s.endsWith("assemblyscript.js")
 )[0];
@@ -21,7 +25,6 @@ if (assemblyscriptPath) {
 }
 const assemblyscript = require(assemblyscriptPath);
 
-//@ts-ignore
 module.exports.Transform = require(transformerPath).Transform;
 module.exports = {
   ...require(ascPath),
