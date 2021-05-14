c2e6a170aa3701ea743601864aad10d2c481b2a3
Jacob Steves <jacob.steves@shopify.com>
2021-05-14 14:26:45 -0400

    run prettier

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,42 +1,51 @@
 import { JSON } from "assemblyscript-json";
 
 export class Variable {
-    key: string;
-    value: string;
+  key: string;
+  value: string;
 
-    constructor(key: string, value: string) {
-        this.key = key;
-        this.value = value;
-    }
+  constructor(key: string, value: string) {
+    this.key = key;
+    this.value = value;
+  }
 }
 
-export function translate(locale: string, key: string, variables: Variable[] = []): string {
-    let jsonObj = <JSON.Obj>JSON.parse(__translations());
+export function translate(
+  locale: string,
+  key: string,
+  variables: Variable[] = []
+): string {
+  let jsonObj = <JSON.Obj>JSON.parse(__translations());
 
-    let translationObj = jsonObj.getObj(locale);
-    if (!translationObj) return key;
+  let translationObj = jsonObj.getObj(locale);
+  if (!translationObj) return key;
 
-    let translation = translationObj.getString(key);
-    if (!translation) return key;
+  let translation = translationObj.getString(key);
+  if (!translation) return key;
 
-    return replaceVariables(translation.toString(), variables);
+  return replaceVariables(translation.toString(), variables);
 }
 
 function __translations(): string {
-    return notImplementedError();
+  return notImplementedError();
 }
 
 function replaceVariables(translation: string, variables: Variable[]): string {
-    let updatedTranslation = translation;
+  let updatedTranslation = translation;
 
-    for (let i: i32 = 0, l: i32 = variables.length; i < l; ++i) {
-        let variable = variables[i];
-        updatedTranslation = updatedTranslation.replace("{" + variable.key + "}", variable.value)
-      }
+  for (let i: i32 = 0, l: i32 = variables.length; i < l; ++i) {
+    let variable = variables[i];
+    updatedTranslation = updatedTranslation.replace(
+      "{" + variable.key + "}",
+      variable.value
+    );
+  }
 
-    return updatedTranslation;
+  return updatedTranslation;
 }
 
 function notImplementedError(): string {
-    throw new Error("You must use the @shopify/rosetta/dist/transform.js compiler transform to use this function.")
+  throw new Error(
+    "You must use the @shopify/rosetta/dist/transform.js compiler transform to use this function."
+  );
 }
--- src/transform.ts
+++ src/transform.ts
@@ -3,7 +3,7 @@ import {
   Parser,
   IdentifierExpression,
   CallExpression,
-  Expression
+  Expression,
 } from "../as";
 import * as path from "path";
 import * as fs from "fs";
@@ -13,33 +13,45 @@ class IncludeBytesTransform extends TransformVisitor {
   private rootDir: string;
 
   visitCallExpression(node: CallExpression): Expression {
-    if (node.expression instanceof IdentifierExpression){
-        if (node.expression.text == "__translations") {
-            let res = SimpleParser.parseExpression(`\`${this.getTranslations()}\``);
-            res.range = node.range;
-            return res;
-        }
+    if (node.expression instanceof IdentifierExpression) {
+      if (node.expression.text == "__translations") {
+        let res = SimpleParser.parseExpression(`\`${this.getTranslations()}\``);
+        res.range = node.range;
+        return res;
+      }
     }
     return super.visitCallExpression(node);
   }
 
   afterParse(parser: Parser): void {
-    this.rootDir = pkgDir.sync(parser.sources.filter(utils.not(utils.isLibrary))[0].normalizedPath) || './';
-    this.visit(parser.sources.filter(s => utils.isLibrary(s) && s.normalizedPath.includes("@shopify/rosetta")));
+    this.rootDir =
+      pkgDir.sync(
+        parser.sources.filter(utils.not(utils.isLibrary))[0].normalizedPath
+      ) || "./";
+    this.visit(
+      parser.sources.filter(
+        (s) =>
+          utils.isLibrary(s) && s.normalizedPath.includes("@shopify/rosetta")
+      )
+    );
   }
 
   getTranslations(): string {
     let config = new Config(this.rootDir);
 
-    var translationMap: { [key: string]: string } = {}
+    var translationMap: { [key: string]: string } = {};
     try {
-    fs.readdirSync(config.translationsPath).forEach((filename: string) => {
+      fs.readdirSync(config.translationsPath).forEach((filename: string) => {
         let language: string = filename.substr(0, filename.lastIndexOf("."));
         let filepath: string = path.join(config.translationsPath, filename);
-        translationMap[language] = JSON.parse(fs.readFileSync(filepath).toString());
-    });
+        translationMap[language] = JSON.parse(
+          fs.readFileSync(filepath).toString()
+        );
+      });
     } catch (e) {
-      console.warn(`[Rosetta] no translations found in '${config.translationsPath}', ${e}`)
+      console.warn(
+        `[Rosetta] no translations found in '${config.translationsPath}', ${e}`
+      );
     }
 
     return JSON.stringify(translationMap);
@@ -47,14 +59,19 @@ class IncludeBytesTransform extends TransformVisitor {
 }
 
 class Config {
-    translationsPath: string;
+  translationsPath: string;
 
-    constructor(projectRootDir: string) {
-        let pathToConfigFile = path.join(projectRootDir, 'i18n.config.json')
-        let configData = (fs.existsSync(pathToConfigFile)) ? JSON.parse(fs.readFileSync(pathToConfigFile).toString()) : {}
+  constructor(projectRootDir: string) {
+    let pathToConfigFile = path.join(projectRootDir, "i18n.config.json");
+    let configData = fs.existsSync(pathToConfigFile)
+      ? JSON.parse(fs.readFileSync(pathToConfigFile).toString())
+      : {};
 
-        this.translationsPath = path.join(projectRootDir, configData.translationsPath || './translations');
-    }
+    this.translationsPath = path.join(
+      projectRootDir,
+      configData.translationsPath || "./translations"
+    );
+  }
 }
 
 export = IncludeBytesTransform;
